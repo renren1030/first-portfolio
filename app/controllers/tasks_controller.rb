6a60c80e6e103f51b266f962d558aaef7ff6c22a
renren1030 <ren.ren.1030@icloud.com>
2020-01-24 23:17:39 +0900

    first commit

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -23,13 +23,17 @@ class TasksController < ApplicationController
 
   def edit
     @task = Task.find(params[:id])
-    
+   
   end
 
 
   def update
     @task = Task.find(params[:id])
-    @task.update(task_params)
+    if @task.update(task_params)
+      redirect_to tasks_path
+    else
+      render action: :edit
+    end
   end
 
   def destroy
